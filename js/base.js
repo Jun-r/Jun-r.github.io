dfa441c601d79fae26106a868cba6872ae171134
Jun-r <348872111@qq.com>
2016-04-15 18:27:28 +0800

    updata js

--- js/base.js
+++ js/base.js
@@ -5,34 +5,31 @@
  */ 
 $(function(){
     var timeArr=[];
-    var browser = {
-        versions : function() {
-            var u = navigator.userAgent, app = navigator.appVersion;
-            return {//移动终端浏览器版本信息
-                trident : u.indexOf('Trident') > -1, //IE内核
-                presto : u.indexOf('Presto') > -1, //opera内核
-                webKit : u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                gecko : u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                mobile : !!u.match(/AppleWebKit.*Mobile.*/)
-                || !!u.match(/AppleWebKit/), //是否为移动终端
-                ios : !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                android : u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-                iPhone : u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器
-                iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp : u.indexOf('Safari') == -1,//是否web应该程序，没有头部与底部
-                google:u.indexOf('Chrome')>-1
-            };
-        }(),
-        language : (navigator.browserLanguage || navigator.language).toLowerCase()
+    var system = {
+        win: false,
+        mac: false,
+        xll: false,
+        ipad:false
+    };
+    //检测平台
+    var p = navigator.platform;
+    system.win = p.indexOf("Win") == 0;
+    system.mac = p.indexOf("Mac") == 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+    system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+    //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+    if (system.win || system.mac || system.xll||system.ipad) {
+       alert("1")
+    } else {
+        alert("2")
     }
-    alert(browser.versions.mobile)
-    if(browser.versions.mobile) {
-        $("[data-url]").on("click", function (ev) {
-            var url = $(this).attr("data-url");
-            ev.preventDefault;
-            window.location.href = url;
-        });
-    }
+    //if(browser.versions.mobile) {
+    //    $("[data-url]").on("click", function (ev) {
+    //        var url = $(this).attr("data-url");
+    //        ev.preventDefault;
+    //        window.location.href = url;
+    //    });
+    //}
     var animationEnd="animationEnd webkitAnimationEnd";
     $("[data-delay]").each(function(i){
         var _this=$(this);
